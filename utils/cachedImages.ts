c1eae921d80a6b6ae1c916a45dc5c7fdc04216f6
wangwei1 <578895238@qq.com>
2023-08-15 18:21:37 +0800

    del env

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -3,7 +3,7 @@ import cloudinary from './cloudinary'
 let cachedResults
 
 export default async function getResults() {
-  if (!cachedResults) {
+  // if (!cachedResults) {
     const fetchedResults = await cloudinary.v2.search
       .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
       .sort_by('public_id', 'desc')
@@ -11,7 +11,7 @@ export default async function getResults() {
       .execute()
 
     cachedResults = fetchedResults
-  }
+  // }
 
   return cachedResults
 }
